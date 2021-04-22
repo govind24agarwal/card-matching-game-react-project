8d653c56396867738807e3c859b70b762906c625
Govind <govindagarwal24@gmail.com>
2021-04-22 19:45:09 +0530

    shuffling cards functionality added

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,14 @@
 const reducer = (state, action) => {
   if (action.type === "LOAD_DATA") {
-    return { ...state, cardData: action.payload };
+    let data = action.payload;
+
+    //Fisher-Yates shuffle
+    for (let i = data.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+      [data[i], data[j]] = [data[j], data[i]];
+    }
+
+    return { ...state, cardData: data };
   }
 
   if (action.type === "FLIP") {
